117e8456f45c9cd02a6a4f72f18198e4eb27d255
AlecadM <71269820+AlexanderMeynik@users.noreply.github.com>
2024-09-10 22:41:20 +0300

    Init project files
    Add docker for database initilaization. Add primary configuration for service application.

---
+++ service/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <pqxx/pqxx>
+#include <ostream>
+#include <fstream>
+std::string res_dir_path = "../res/";
+std::string filename = res_dir_path.append("config.txt");
+int main() {
+    std::ifstream conf(filename);
+    std::string dbname,user,password;
+    conf>>dbname>>user>>password;
+    std::string cstring="dbname="+dbname+" user="+user+" password="+password+" host=localhost port=5432";
+    try {
+        pqxx::connection C(cstring);
+        if (C.is_open()) {
+            std::cout << "Opened database successfully: " << C.dbname() << std::endl;
+        } else {
+            std::cerr << "Connection failed" << std::endl;
+        }
+    } catch (const std::exception &e) {
+        std::cerr << "Exception: " << e.what() << std::endl;
+    }
+
+    return 0;
+}
