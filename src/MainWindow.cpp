400c8a0e5574815d9bc964dae3ea7241d24c61e4
AlecadM <71269820+AlexanderMeynik@users.noreply.github.com>
2024-10-23 20:21:53 +0300

    Add more groups and elements.

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -9,17 +9,12 @@
 
 std::initializer_list<QString> ll = {"2", "4", "8", "16", "32"};//todo remake
 
-/*std::initializer_list<QString>  hashes={ "sha224",
-                                         "sha256",
-                                         "md5",
-                                         "sha384",
-                                         "sha512"};//todo remake*/
 namespace windows {
 
 
     MainWindow::MainWindow(QWidget *parent)
             : QMainWindow(parent) {
-
+        this->setFixedSize(1024, 620);
         setupUI();
 
         connect(importButton, &QPushButton::pressed, this, &MainWindow::onImport);
@@ -46,24 +41,24 @@ namespace windows {
     void MainWindow::setupUI() {
         setWindowTitle("File Processing Application");
 
-        centralWidget = new QWidget(this);
-        setCentralWidget(centralWidget);
+        setCentralWidget(new QWidget());
+        auto cent = centralWidget();
 
 
-        mainLayout = new QVBoxLayout(centralWidget);
+        auto mmm = new QHBoxLayout();
+        mainLayout = new QVBoxLayout();
 
-        inputFile = new FileLineEditWithOption(this, QDir().currentPath());
-
-        outputFile = new FileLineEditWithOption(this, QDir().currentPath(), true);
+        inputFile = new FileLineEditWithOption(cent, QDir::currentPath());
+        outputFile = new FileLineEditWithOption(cent, QDir::currentPath(), true);
 
 
         optionsLayout = new QHBoxLayout();
-        QLabel *labelHashFunction = new QLabel("Hash Function:", this);
-        QLabel *labelSegmentSize = new QLabel("Segment Size:", this);
+        labelHashFunction = new QLabel("Hash Function:");
+        labelSegmentSize = new QLabel("Segment Size:");
 
 
-        hashComboBox = new QComboBox(this);
-        segmentSizeComboBox = new QComboBox(this);
+        hashComboBox = new QComboBox(cent);
+        segmentSizeComboBox = new QComboBox(cent);
 
         optionsLayout->addWidget(labelHashFunction);
         optionsLayout->addWidget(hashComboBox);
@@ -71,26 +66,27 @@ namespace windows {
         optionsLayout->addWidget(segmentSizeComboBox);
 
         buttonLayout = new QHBoxLayout();
-        importButton = new QPushButton("Import", this);
-        exportButton = new QPushButton("Export", this);
-        deleteButton = new QPushButton("Delete", this);
+        importButton = new QPushButton("Import");
+
 
         buttonLayout->addWidget(importButton);
-        buttonLayout->addWidget(exportButton);
-        buttonLayout->addWidget(deleteButton);
 
 
-
-
-        /*settingsButton = new QPushButton("Settings", this);*/
-        /*buttonLayout->addWidget(settingsButton);*/
-
-
-        auto hbl = new QHBoxLayout();
         settingsAction = new QAction("Settings");
         menuBar()->addAction(settingsAction);
 
 
+        exportButton = new QPushButton("Export");
+        deleteButton = new QPushButton("Delete");
+
+        hbl = new QHBoxLayout();
+        fileExportField = new QComboBox();
+        hbl->addWidget(exportButton);
+        hbl->addWidget(deleteButton);
+
+        //todo add database status(total blocks files size) files
+        //todo for import and export add fileds for segment count etc.
+        //todo add connection qled
 
 
         //todo use tree or list model
@@ -99,31 +95,97 @@ namespace windows {
         //todo can ve create our own model using pqxx res
         /*QSqlQueryModel aa;*/
 
-        dataTable = new QTableWidget(this);
+        dataTable = new QTableWidget();
         dataTable->setColumnCount(3);
         QStringList headers;
         headers << "File" << "Hash" << "Size";
         dataTable->setHorizontalHeaderLabels(headers);
 
-        logTextField = new QTextEdit(this);
+        logTextField = new QTextEdit();
         logTextField->setReadOnly(true);
 
         mainLayout->addWidget(inputFile);
-        mainLayout->addWidget(outputFile);
         mainLayout->addLayout(optionsLayout);
         mainLayout->addLayout(buttonLayout);
         mainLayout->addWidget(dataTable);
-        mainLayout->addWidget(logTextField);
+        mainLayout->addWidget(fileExportField);
+        mainLayout->addWidget(outputFile);
+        mainLayout->addLayout(hbl);
+        mmm->addLayout(mainLayout);
+
+        auto VV = new QVBoxLayout();
+
+
+        includeOptionsArea = new QGroupBox(tr("FileImportOptions"));
+        replaceCB = new QCheckBox();
+        replaceCB->setToolTip("If checked will replace file contents");
+        auto incudeOptionLay = new QGridLayout();
+
+        numberBlocks = new QLCDNumber();
+        totalSize = new QLCDNumber();
+        numbErSegments = new QLCDNumber();
+        totalRepeatedBlocks = new QLCDNumber();
+        totalRepeatedpercentage = new QLCDNumber();
+        importTime = new QLCDNumber();
+
+        incudeOptionLay->addWidget(new QLabel("Replace files on import"), 0, 0, 1, 2);
+        incudeOptionLay->addWidget(replaceCB, 0, 2, 1, 1);
+
+
+        incudeOptionLay->addWidget(new QLabel("Load time (ms)"),
+                                   0, 3, 1, 1);
+        incudeOptionLay->addWidget(importTime,
+                                   0, 4, 1, 2);
+
+
+        incudeOptionLay->addWidget(new QLabel("Total file blocks"), 1, 0, 1, 1);
+        incudeOptionLay->addWidget(numberBlocks, 1, 1, 1, 1);
+        incudeOptionLay->addWidget(new QLabel("Total file size"), 1, 2, 1, 1);
+        incudeOptionLay->addWidget(totalSize, 1, 3, 1, 3);
+
+
+        incudeOptionLay->addWidget(new QLabel("Files segment count"), 2, 0, 1, 1);
+        incudeOptionLay->addWidget(numbErSegments, 2, 1, 1, 1);
+
+        incudeOptionLay->addWidget(new QLabel("Duplicated blocks count"), 2, 2, 1, 1);
+        incudeOptionLay->addWidget(totalRepeatedBlocks, 2, 3, 1, 1);
+
+        incudeOptionLay->addWidget(totalRepeatedpercentage, 2, 4, 1, 1);
+        incudeOptionLay->addWidget(new QLabel("%"), 2, 5, 1, 1);
+
+        includeOptionsArea->setLayout(incudeOptionLay);
+
+
+        auto exportOptionsArea = new QGroupBox(tr("FileExportOptions"));
+        deleteFiles = new QCheckBox();
+        deleteFiles->setToolTip("If checked will delete file/directory after export.");
+        createMain = new QCheckBox();
+        createMain->setToolTip("If checked  new directory will be created if out path does not exist!");
+        auto exportOptionLay = new QGridLayout();
+        exportOptionLay->addWidget(deleteFiles);
+        exportOptionLay->addWidget(createMain);
+        exportOptionsArea->setLayout(exportOptionLay);
+
+
+        VV->addWidget(includeOptionsArea);
+        VV->addWidget(logTextField);
+        VV->addWidget(exportOptionsArea);
+
+
+        mmm->addLayout(VV);
+
+        /*mainLayout->addWidget(logTextField);*/
+        centralWidget()->setLayout(mmm);
     }
 
     void MainWindow::onImport() {
         QString inputPath = inputFile->getContent();
         size_t segmentSize = segmentSizeComboBox->currentText().toUInt();
         if (QFileInfo(inputPath).isDir()) {
-            qInfo("process directory");
+            writeLog("process directory");
             //fileService.processDirectory(inputPath.toStdString(), segmentSize);
         } else {
-            qInfo("process file");
+            writeLog("process file");
             //fileService.processFile(inputPath.toStdString(), segmentSize);
         }
     }
@@ -133,10 +195,10 @@ namespace windows {
         QString outputPath = outputFile->getContent();
         QString fromPath = inputFile->getContent();
         if (QFileInfo(fromPath).isDir()) {
-            qInfo("load directory");
+            writeLog("load directory");
             //fileService.loadDirectory(fromPath.toStdString(), outputPath.toStdString());
         } else {
-            qInfo("load file");
+            writeLog("load file");
             // fileService.loadFile(fromPath.toStdString(), outputPath.toStdString());
         }
     }
@@ -145,10 +207,10 @@ namespace windows {
         // Example of calling deleteFile or deleteDirectory from FileParsingService
         QString path = inputFile->getContent();
         if (QFileInfo(path).isDir()) {
-            qInfo("delete directory");
+            writeLog("delete directory");
             //fileService.deleteDirectory(path.toStdString());
         } else {
-            qInfo("delete file");
+            writeLog("delete file");
             //fileService.deleteFile(path.toStdString());
         }
     }
@@ -157,15 +219,15 @@ namespace windows {
 
         SettingsWindow settingsWindow(this);
         auto stat = settingsWindow.exec();
-        qInfo(std::to_string(stat).c_str());
+        writeLog(std::to_string(stat).c_str());
         if (stat == QDialog::Accepted) {
             c_str = std::move(settingsWindow.getConfiguration());
-            qInfo("update settings");
+            writeLog("update settings");
         }
 
         settingsWindow.close();
 
-        qInfo(c_str.c_str());
+        writeLog(c_str.c_str());
     }
 
     void MainWindow::chekConditions() {
@@ -182,5 +244,10 @@ namespace windows {
         exportButton->setEnabled(!in.isEmpty() && !out.isEmpty()
                                  && cond);
         deleteButton->setEnabled(!in.isEmpty());
+    }
+
+    void MainWindow::writeLog(QString qss, LogLevel lg) {
+        logTextField->setTextColor(colourLookUp[lg]);
+        logTextField->append(QString(logLevelLookUp[lg]).arg(qss));
     }
 }
--- src/MainWindow.h
+++ src/MainWindow.h
@@ -11,6 +11,9 @@
 #include <QVBoxLayout>
 #include <QHBoxLayout>
 #include <QLabel>
+#include <QGroupBox>
+
+#include <QLCDNumber>
 
 #include "FileService.h"
 #include "FileLineEdit.h"
@@ -18,12 +21,34 @@
 
 namespace windows {
 
+    enum LogLevel
+    {
+        INFO,
+        WARNING,
+        ERROR,
+        RESULT
+    };
 
+    static constexpr std::array<const char*,4> logLevelLookUp=
+            {
+                    "[INFO] %1",
+                    "[WARNING] %1",
+                    "[ERROR] %1",
+                    "[RESULT] %1",
+            };
+    static constexpr std::array<Qt::GlobalColor,4> colourLookUp
+{
+        Qt::black,
+        Qt::darkYellow,
+        Qt::red,
+        Qt::darkGreen
+};
     class MainWindow : public QMainWindow {
     Q_OBJECT
 
     public:
         explicit MainWindow(QWidget *parent = nullptr);
+
 
         ~MainWindow() override;
 
@@ -39,8 +64,9 @@ namespace windows {
 
         void onSettings();
 
+        void writeLog(QString qss,LogLevel lg=RESULT);
+    //todo at osme point we need either to create or to use existing database
     private:
-        QWidget *centralWidget;
 
         FileLineEditWithOption *inputFile;
         FileLineEditWithOption *outputFile;
@@ -50,7 +76,6 @@ namespace windows {
         QPushButton *importButton;
         QPushButton *exportButton;
         QPushButton *deleteButton;
-        /* QPushButton *settingsButton;*/
         QTableWidget *dataTable;
         QTextEdit *logTextField;
 
@@ -58,12 +83,32 @@ namespace windows {
 
         QHBoxLayout *optionsLayout;
         QHBoxLayout *buttonLayout;
+        QHBoxLayout *hbl;
 
+        QComboBox *fileExportField;
 
         QAction *settingsAction;
 
+        QLabel *labelSegmentSize;
+        QLabel *labelHashFunction;
         /*file_services::FileParsingService fileService;*/
         db_services::myConnString c_str;
+
+        QGroupBox* includeOptionsArea;
+
+        QCheckBox *replaceCB;
+        QCheckBox* createMain;
+        QCheckBox* deleteFiles;
+
+        QLCDNumber * numberBlocks;
+        QLCDNumber * totalSize;
+        QLCDNumber * numbErSegments;
+        QLCDNumber * totalRepeatedBlocks;
+        QLCDNumber * totalRepeatedpercentage;
+
+        QLCDNumber * importTime;
+
+        QLCDNumber * exportTime;
 
         void setupUI();
     };
